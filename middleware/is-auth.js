09ef8efdebf905eefb8f1f4369eb86b432b43b97
Mohammad <mmdfrn81@gmail.com>
2021-03-21 18:23:59 +0330

    util/flashError added

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,10 +1,15 @@
-const { flashError } = require("../util/error");
+const { flashError } = require('../util/error');
 
 module.exports = (req, res, next) => {
     const user = req.user;
     // if user not found = user not log in
-    if(!user) {
-        flashError(req, res, 'لطفا ابتدا وارد حساب خود شوید', '/auth/login');
+    if (!user) {
+        return flashError(
+            req,
+            res,
+            'لطفا ابتدا وارد حساب خود شوید',
+            '/auth/login'
+        );
     }
     next();
-}
+};
