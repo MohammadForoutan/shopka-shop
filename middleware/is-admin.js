99fa3e9f237a5dc3f002c8ac719c2315fba80569
MohammadForoutan <mmdfrn81@gmail.com>
2021-04-20 04:54:17 +0430

    bug-fix: when cookie max-age finished

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -1,10 +1,18 @@
-const { flashError } = require("../util/error");
+const { flashError } = require('../util/error');
 
 module.exports = (req, res, next) => {
     const user = req.user;
+    if (!user) {
+        return flashError(
+            req,
+            res,
+            'لطفا ابتدا وارد حساب خود شوید',
+            '/auth/login'
+        );
+    }
     // if user not found = user not log in
-    if(user.accessLevelId < 2) {
-        flashError(req,res,'شما ادمین نیستی داداش', '/' );
+    if (user.accessLevelId < 2) {
+        flashError(req, res, 'شما ادمین نیستی داداش', '/');
     }
     next();
-}
+};
